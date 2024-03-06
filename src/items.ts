7aedcb86c48b6837903c04a5c85b8979f12b3031
tone <ytoune@users.noreply.github.com>
2024-03-06 23:30:49 +0900

    mod

--- src/items.ts
+++ src/items.ts
@@ -5,23 +5,30 @@ import { isArray } from './utils'
 import type { Scroll } from './with-scroll'
 
 const positionsCaches = new WeakMap<readonly number[], ReadonlySet<number>>()
+const getCached = (k: readonly number[]) => {
+  let v = positionsCaches.get(k)
+  if (!v) positionsCaches.set(k, (v = new Set(k)))
+  return v
+}
 
-const iter = function* ([b, e]: readonly [number, number], s?: StickyPosition) {
-  if (void 0 === s) {
-    for (let i = b; i < e; ++i) yield [i, void 0] as const
-    return
+const createIter = (
+  [b, e]: readonly [number, number],
+  s?: StickyPosition,
+): ((cb: (i: number, p?: number) => void) => void) => {
+  if (void 0 === s)
+    return (cb: (i: number) => void) => {
+      for (let i = b; i < e; ++i) cb(i)
+    }
+  if ('number' === typeof s)
+    return (cb: (i: number, p?: number) => void) => {
+      for (let i = b; i < e; ++i) if (s < i) cb(i)
+      for (let i = 0; i <= s; ++i) cb(i, i)
+    }
+  const u: ReadonlySet<number> = isArray(s) ? getCached(s) : s
+  return (cb: (i: number, p?: number) => void) => {
+    for (let i = b; i < e; ++i) if (!u.has(i)) cb(i)
+    for (const i of s) cb(i, i)
   }
-  if ('number' === typeof s) {
-    for (let i = b; i < e; ++i) if (s < i) yield [i, void 0] as const
-    for (let i = 0; i <= s; ++i) yield [i, i] as const
-    return
-  }
-  let u: ReadonlySet<number> | undefined
-  if (isArray(s))
-    (u = positionsCaches.get(s)) || positionsCaches.set(s, (u = new Set(s)))
-  else u = s
-  for (let i = b; i < e; ++i) if (!u.has(i)) yield [i, void 0] as const
-  for (const i of s) yield [i, i] as const
 }
 
 export const createItems = <T>(
@@ -32,29 +39,34 @@ export const createItems = <T>(
   overscanCount = 20,
 ): T[] => {
   const { colSizes, rowSizes, cell } = format
-  const colCount = colSizes.length
-  const rowCount = rowSizes.length
   const items: T[] = []
-  if (rowCount && colCount) {
-    const rowRange = getRange(
-      rowSizes,
-      scroll.clientHeight,
-      scroll.top,
-      scroll.topDirection,
-      overscanCount,
+  if (rowSizes.length && colSizes.length) {
+    const rowIter = createIter(
+      getRange(
+        rowSizes,
+        scroll.clientHeight,
+        scroll.top,
+        scroll.topDirection,
+        overscanCount,
+      ),
+      sticky?.r,
     )
-    const colRange = getRange(
-      colSizes,
-      scroll.clientWidth,
-      scroll.left,
-      scroll.leftDirection,
-      overscanCount,
+    const colIter = createIter(
+      getRange(
+        colSizes,
+        scroll.clientWidth,
+        scroll.left,
+        scroll.leftDirection,
+        overscanCount,
+      ),
+      sticky?.c,
     )
-    for (const [r, q] of iter(rowRange, sticky?.r))
-      for (const [c, p] of iter(colRange, sticky?.c)) {
+    rowIter((r, q) => {
+      colIter((c, p) => {
         const el = renderItem(r, c, cell([r, c], { c: p, r: q }))
         if (el) items.push(el)
-      }
+      })
+    })
   }
   return items
 }
