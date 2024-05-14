03cf6b2994aa22afd36bcb77bd918aad7e4c4b59
tone <ytoune@users.noreply.github.com>
2024-05-14 15:28:35 +0900

    test: mod

--- src/controller.test.ts
+++ src/controller.test.ts
@@ -78,4 +78,75 @@ origin に current を代入して再描画
       gridConst: 2,
     })
   })
+
+  it('固定が上にある場合', () => {
+    let container = null as null | ScrollContainer
+    const ref = () => container
+    const sizes = { length: 21, size: 10 } as const
+    const sticky = [1] // 2 行目は固定されていて常に表示
+    const initOffset = () => 0
+    const defaultPageSize = () => 20
+    container = null
+    const controller = createController({
+      ref,
+      sizes,
+      sticky,
+      initOffset,
+      defaultPageSize,
+    })
+    // 0  1  2  3  4  5  6  7  8  9 ..
+    //      20    40    60
+    expect(controller.state()).toEqual({
+      offset: 0,
+      pageSize: 20,
+      realOffset: 0,
+    })
+    expect(controller.render()).toEqual({
+      range: [0, 6],
+      innerSize: 60,
+      gridTemplate: '0px repeat(6, 10px)',
+      gridConst: 2,
+    })
+    // 少しだけスクロールする
+    container = { offset: 15, pageSize: 20 }
+    controller.recalc()
+    expect(controller.state()).toEqual({
+      offset: 15,
+      pageSize: 20,
+      realOffset: 15,
+    })
+    // 閾値を超えるまでは描画範囲はそのまま
+    expect(controller.render()).toEqual({
+      range: [0, 6],
+      innerSize: 60,
+      gridTemplate: '0px repeat(6, 10px)',
+      gridConst: 2,
+    })
+    // スクロール量がビューポートサイズを超えた
+    container = { offset: 25, pageSize: 20 }
+    controller.recalc()
+    // 0  1  2  3  4  5  6  7  8  9 ..
+    //      20    40    60    80
+    expect(controller.state()).toEqual({
+      offset: 25,
+      pageSize: 20,
+      realOffset: 25,
+    })
+    expect(controller.render()).toEqual({
+      range: [0, 9],
+      innerSize: 85,
+      gridTemplate: '0px repeat(9, 10px)',
+      gridConst: 2,
+    })
+    // 大きくスクロール
+    container = { offset: 70, pageSize: 20 }
+    controller.recalc()
+    // 0  1  2  3  4  5  6  7  8  9 10 11 ..
+    //          0    20    40    60    80
+    expect(controller.state()).toEqual({
+      offset: 70,
+      pageSize: 20,
+      realOffset: 40,
+    })
+  })
 })
